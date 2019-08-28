a9e90fd4bf6ac1a3e396509556beaef86edffe6b
bprymicz <bradprymicz@gmail.com>
2019-08-28 11:47:05 -0400

    adding test file

---
+++ src/test.ts
@@ -0,0 +1,22 @@
+import Emojify from "./Controllers/Emojify";
+var emoji = new Emojify();
+
+var exampleString = "knife :knife1: :knife2: :knife3: :knife4: :knife5:";
+//  console.log(emoji.parseRequest(exampleString));
+// console.log(emoji.process(exampleString));
+//               012345678
+exampleString = "this is a good sentence :no:";
+// console.log(emoji.parseRequest(exampleString));
+console.log(emoji.process(exampleString));
+// exampleString = "hello :no::killer: you jerk ";
+// console.log(emoji.parseRequest(exampleString));
+// console.log(emoji.process(exampleString));
+// exampleString = "help";
+// console.log(emoji.parseRequest(exampleString));
+// console.log(emoji.process(exampleString));
+// exampleString = "help me";
+// console.log(emoji.parseRequest(exampleString));
+// console.log(emoji.process(exampleString));
+// exampleString = "help me > :no:";
+// console.log(emoji.parseRequest(exampleString));
+// console.log(emoji.process(exampleString));
